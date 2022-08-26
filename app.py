9ec08b5f39c32e22cfcceae8adff43b8ee6a4c47
АланиКзереб <ale.berezckina@yandex.ru>
2022-08-27 00:18:51 +0300

    changed jsonfile

--- app.py
+++ app.py
@@ -1,36 +1,22 @@
+import logging
+
 from flask import Flask, request, render_template, send_from_directory
 # from functions import ...
+from lesson12_project_source_v3.loader.views import loader_blueprint
+from lesson12_project_source_v3.main.views import main_blueprint
 
-POST_PATH = "posts.json"
+POSTS_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
 
-@app.route("/")
-def page_index():
-    pass
-
-
-@app.route("/list")
-def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
-
-
+logging.basicConfig(filename="basic.log", level=logging.INFO)
 @app.route("/uploads/<path:path>")
 def static_dir(path):
     return send_from_directory("uploads", path)
 
 
 app.run()
-
---
+++ main/views.py
@@ -0,0 +1,27 @@
+from json import JSONDecodeError
+
+from flask import Flask, Blueprint, render_template, request
+
+from lesson12_project_source_v3.functions import get_posts_by_words
+
+main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates_name')
+
+
+@main_blueprint.route('/')
+def main_page():
+    return render_template('index.html')
+
+
+@main_blueprint.route('/search/')
+def search_page():
+    search_query = request.args.get('s', '')
+    try:
+        posts = get_posts_by_words(search_query)
+    except FileNotFoundError:
+        # logging.info('Файл не найден')
+        return 'Файл не найден'
+    except JSONDecodeError:
+        return 'Невалидный файл'
+
+    return render_template('post_list.html', query=search_query, posts=posts)
+
